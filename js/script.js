d78b4d511497aadfd3f35e847a4090e7f9212793
radhy shyam <radhy2929@gmail.com>
2026-02-05 20:45:23 +0530

    resume updaed

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
 console.log("Portfolio Loaded Successfully");
 document.getElementById("year").textContent = new Date().getFullYear();
 console.log('everything is up to date')
+console.log('resume updated')
